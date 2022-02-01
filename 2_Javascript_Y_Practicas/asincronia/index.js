05d80d2e4a413952380871dea238d640b7263d02
j.montoya <j.montoya@bbva.com>
2022-01-31 21:37:37 -0600

    feat: primeros ejemplos de asincronia

---
+++ 2_Javascript_Y_Practicas/asincronia/index.js
@@ -0,0 +1,36 @@
+// Codigo ejecutado linea tras linea por el eventloop
+/*
+console.log('1 Inicio');
+console.log('2 Ejecutando');
+console.log('3 Terminando');
+*/
+
+// Ejecucion que toma tiempo siempre va al event queue
+/*
+console.log('1 Inicio');
+setTimeout(() => {
+    console.log('2 Ejecutando');
+}, 0000)
+console.log('3 Terminando');
+*/
+
+function run() {
+    console.log('1 Inicio');
+    myFuncion(ejecutando);
+    console.log('4 Fin');
+}
+
+function myFuncion(ejecutando) {
+    setTimeout(() => {
+        console.log('2 Ejecutando');
+        ejecutando();
+        
+    }, 0000)
+}
+
+function ejecutando() {
+    console.log('3 Terminando');
+    
+}
+
+run();
