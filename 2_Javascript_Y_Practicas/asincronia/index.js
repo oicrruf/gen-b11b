2806e8c17e087ad079125f67f25fbc17bf3f58da
j.montoya <j.montoya@bbva.com>
2022-02-01 19:04:30 -0600

    feat: bloque 4 de asincronia, usando promesas
    Ejemplo de manejo de promesas con then, catch y finally

--- 2_Javascript_Y_Practicas/asincronia/index.js
+++ 2_Javascript_Y_Practicas/asincronia/index.js
@@ -15,14 +15,15 @@ console.log('3 Terminando');
 */
 
 // Solucion 1 Callbacks (NO ES RECOMENDADA por casos de callbackhell)
+/*
 function run() {
     console.log('1 Inicio');
     myFuncion(ejecutando);
-    console.log('4 Fin');
+    // console.log('4 Fin');
 }
 
-function myFuncion(ejecutando) {
-    setTimeout(() => {
+function myFuncion(ejecutando) { 
+    setTimeout(() => { // setTimeout es una simulacionde operaciones que toman tiempo (conexiones a BD, consumo de API)
         console.log('2 Ejecutando');
         ejecutando();
     }, 0000)
@@ -30,6 +31,36 @@ function myFuncion(ejecutando) {
 
 function ejecutando() {
     console.log('3 Terminando');
+    console.log('4 Fin');
 }
 
 run();
+*/
+
+// Solucion 2 Promesas
+// Promise: Algo que va a ocurrir pero no sabemos cuando va a ocurrir.
+// Estados de las promesas: Pendiente (pending), resuelto (resolve) y rechazado (reject)
+function asincrona() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('2 Ejecutando');
+            resolve();
+            // reject(new Error('mi custom error'));
+        }, 1000)
+    });
+}
+
+console.log('1 Inicio');
+asincrona().then(() => {
+    // Siguientes acciones despues de que responde la promesa con resolve()
+    console.log('en el then');
+    console.log('3 Terminando');
+}).catch(error => { 
+    // Siguientes acciones despues de que responde la promesa con reject()
+    console.log('error: ', error)
+})
+.finally(() => {
+    // Se ejecuta siempre, tanto si la promesa fue resolve o reject
+    console.log('4 FIN');
+    console.log('en el finally')
+})
