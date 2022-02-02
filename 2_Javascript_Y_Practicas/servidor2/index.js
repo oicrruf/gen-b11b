015641ff09b4eb70f2c6d2585f780d3a38521c2e
j.montoya <j.montoya@bbva.com>
2022-02-01 20:57:22 -0600

    feat: ejemplo de servidor2

---
+++ 2_Javascript_Y_Practicas/servidor2/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const urlObject = url.parse(req.url);
+    console.log(`Path: ${urlObject.path}`)
+    console.log(`Path name: ${urlObject.pathname}`)
+    res.writeHead(200, { 'Content-Type' : 'text/html' } );
+    res.write(`<html><body>Servidor 2</body></html>`);
+    res.end();
+})
+
+server.listen(8080);
+console.log('Inicie mi servidor');
