dcd59622cb4c23c70d2565d805765918e08baca3
j.montoya <j.montoya@bbva.com>
2022-02-08 19:13:11 -0600

    FEAT: Request y query params

--- 3_Express/pokeapi-express/src/index.js
+++ 3_Express/pokeapi-express/src/index.js
@@ -6,9 +6,31 @@ app.get("/", (req, res) => {
 	res.send("hola mundo!");
 });
 
-app.get("/trainer", (req, patito) => {
+app.get("/trainer", (req, res) => {
 	const objeto = { id: 1, name: "jose montoya", age: 32 };
-	patito.json(objeto);
+	res.json(objeto);
+});
+
+// Request Param: Ejecutar una operaciones sobre un elemento especifico
+// http://localhost:3000/trainers?page=34&size=12
+app.get("/trainer/:idTrainer/region/:idRegion", (req, res) => {
+	const { idTrainer, idRegion } = req.params; 
+	const objeto = { id: idTrainer, name: "ash ketchum", age: 10, region: idRegion };
+	res.json(objeto);
+});
+
+// Query Params: Filtrar la informacion
+// http://localhost:3000/trainers?page=34&size=12
+app.get("/trainers", (req, res) => {
+	// const page = req.query.page;
+	// const size = req.query.size;
+	// { page: 1, size: 10 }
+	const { page, size } = req.query; 
+	if (page && size) {
+		res.json({ page, size });
+	} else {
+		res.send('Parametro obligatorios');
+	}
 });
 
 app.listen(port, () => {
