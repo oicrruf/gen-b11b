566c25a681ce8e2a13017d726488b5fb56aa7ef4
oicrruf <vm.reyesal@gmail.com>
2022-02-14 20:23:48 -0600

    feat: get pokemons

--- 4_EJS/routes/index.js
+++ 4_EJS/routes/index.js
@@ -3,29 +3,47 @@ var router = express.Router();
 var axios = require("axios");
 
 /* GET home page. */
+
 router.get("/", function (req, res, next) {
-  // axios({
-  //   method: "get",
-  //   url: "https://jsonplaceholder.typicode.com/todos/2",
-  // }).then(function (response) {
-  //   console.log(response.data);
-  // });
+  res.render("index", {
+    title: "Inicio",
+    message: "Bienvenido",
+  });
+});
 
-  // axios({
-  //   url: "https://jsonplaceholder.typicode.com/todos/2",
-  // }).then(function (response) {
-  //   console.log(response.data);
-  // });
-
-  axios
-    .get("https://pokeapi.co/api/v2/pokemon/287")
-    .then(function (response) {
-
-
-
-      console.log(response.data)
-      res.render("index", response.data);
+router.get("/pokemon/:name", function (req, res, next) {
+  let { name } = req.params;
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
+    console.log(response)
+    res.render("pokemon", {
+      title: name,
+      
+      data: response.data,
     });
+  });
+});
+
+router.get("/todos", function (req, res, next) {
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+    console.log(response.data);
+    res.render("todos", {
+      title: "Todos",
+      data: response.data,
+    });
+  });
 });
 
 module.exports = router;
+
+// axios({
+//   method: "get",
+//   url: "https://jsonplaceholder.typicode.com/todos/2",
+// }).then(function (response) {
+//   console.log(response.data);
+// });
+
+// axios({
+//   url: "https://jsonplaceholder.typicode.com/todos/2",
+// }).then(function (response) {
+//   console.log(response.data);
+// });
